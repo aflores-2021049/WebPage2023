53e38720862aa4dfa2d67a5e31b66e60b2579548
Andres Flores <aflores-2021049@kinal.edu.gt>
2023-01-20 09:37:46 -0600

    V1 commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function saludar(){
+
+
+
+    let numA = document.getElementById('valorA').value;
+
+    let numB = document.getElementById('valorB').value;
+
+    let resultado = parseInt(numA) + parseInt(numB);
+
+
+
+    console.log(saludar);
+
+    console.log(resultado);
+
+}
